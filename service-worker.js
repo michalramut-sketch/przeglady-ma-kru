af06fe9f41fa36aa22e7cee0d45365e5cb141658
michalramut-sketch <michalramut@gmail.com>
2026-01-12 22:18:06 +0100

    Register service worker in service-worker.js
    Added script to register the service worker.

--- service-worker.js
+++ service-worker.js
@@ -1,2 +1,9 @@
+<!--
+<script>
+if('serviceWorker' in navigator){
+ navigator.serviceWorker.register('./service-worker.js');
+}
+</script>
+-->
 self.addEventListener('install',e=>self.skipWaiting());
 self.addEventListener('fetch',e=>{});
