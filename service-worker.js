0b710ebd1d262d1efac8ab365d2a37f67f2c8d96
michalramut-sketch <michalramut@gmail.com>
2026-01-12 23:19:59 +0100

    Clean up service-worker.js by removing comments
    Removed commented-out script for service worker registration.

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,4 @@
-<!--
-<script>
-if('serviceWorker' in navigator){
- navigator.serviceWorker.register('./service-worker.js');
-}
-</script>
+
 -->
 self.addEventListener('install',e=>self.skipWaiting());
 self.addEventListener('fetch',e=>{});
