5dd79e620c590bb9d6c513eb350cb020cbc0f9d6
michalramut-sketch <michalramut@gmail.com>
2026-01-12 19:29:51 +0100

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,6 @@
+function showBuildings(){
+document.getElementById('content').innerHTML='<h3>Lista budynków</h3><ul><li>Armii Krajowej 2</li><li>Kościuszki 5</li></ul>';
+}
+function newInspection(){
+document.getElementById('content').innerHTML='<h3>Nowy przegląd</h3><p>Formularz przeglądu</p>';
+}
