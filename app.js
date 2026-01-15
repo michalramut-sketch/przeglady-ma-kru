ce215f682e1b066e9de9d095405b2b77a831d847
michalramut-sketch <michalramut@gmail.com>
2026-01-15 16:11:47 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,14 @@
+document.getElementById('app').innerHTML = `
+<section>
+<h2>Sekcja C – test</h2>
+<label>Stan:
+<select>
+<option selected>Zadowalający</option>
+<option>Usterki</option>
+</select>
+</label><br><br>
+<label>Uwagi:<br>
+<textarea rows="4" cols="40"></textarea>
+</label>
+</section>
+`;
