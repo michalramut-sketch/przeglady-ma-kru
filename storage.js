d8e13986f4dd62b31105c548769ac2cd853892a1
michalramut-sketch <michalramut@gmail.com>
2026-01-16 18:24:58 +0100

    Delete storage.js

--- storage.js
+++
@@ -1,8 +0,0 @@
-function saveInspection(obj){
- const list = JSON.parse(localStorage.getItem("inspections")||"[]");
- list.push(obj);
- localStorage.setItem("inspections",JSON.stringify(list));
-}
-function loadInspections(){
- return JSON.parse(localStorage.getItem("inspections")||"[]");
-}
